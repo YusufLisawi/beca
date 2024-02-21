3219e04754efa100f7c530a0c79950d3f51f3260
YusufLisawi <yusufisawi@gmail.com>
2024-02-21 22:06:40 +0100

    Updates

--- orm/hibernate/src/main/java/org/nttdata/dao/ProduitDAO.java
+++ orm/hibernate/src/main/java/org/nttdata/dao/ProduitDAO.java
@@ -9,7 +9,6 @@ public interface ProduitDAO {
 	public void addProduit(Produit produit);
 	public void updateProduit(Produit produit);
 	public List<Produit> listProduits();
-//	public List<Produit> selectCatByKeyword(String keyWord);
 	public Produit getProduitById(int id);
 	public void removeProduit(int id);
 }
