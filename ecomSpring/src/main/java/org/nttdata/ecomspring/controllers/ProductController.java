a84fd885e246ec45f76e006ce1ed8577926ea52b
YusufLisawi <yusufisawi@gmail.com>
2024-04-04 11:07:50 +0000

    Updates

--- ecomSpring/src/main/java/org/nttdata/ecomspring/controllers/ProductController.java
+++ ecomSpring/src/main/java/org/nttdata/ecomspring/controllers/ProductController.java
@@ -1,25 +1,43 @@
 package org.nttdata.ecomspring.controllers;
 
+import org.nttdata.ecomspring.entities.Category;
 import org.nttdata.ecomspring.entities.Product;
+import org.nttdata.ecomspring.services.CategoryService;
 import org.nttdata.ecomspring.services.ProductService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class ProductController {
-    private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public static String UPLOAD_PATH = "uploads" + File.separator;
+    private static final String UPLOAD_DIRECTORY = "classpath:static/uploads/";
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+    private final ResourceLoader resourceLoader;
+
+    public ProductController(ProductService productService, CategoryService categoryService, ResourceLoader resourceLoader) {
         this.productService = productService;
+        this.categoryService = categoryService;
+        this.resourceLoader = resourceLoader;
     }
 
     @GetMapping("/products")
     public String getProducts(Model model) {
-        List<Product> products = productService.getAllProducts();
-        model.addAttribute("products", products);
+        addCategoriesToModel(model);
+        addProductsToModel(model);
         return "products";
     }
 
@@ -27,20 +45,16 @@ public class ProductController {
     public String getProductById(@PathVariable(value = "id") Long id, Model model) {
         Product product = productService.getProductById(id);
         model.addAttribute("product", product);
-        List<Product> products = productService.getAllProducts();
-        model.addAttribute("products", products);
+        addCategoriesToModel(model);
+        addProductsToModel(model);
         return "products";
     }
 
     @PostMapping("/products")
-    public String addProduct(Product product) {
+    public String addProduct(Product product, @RequestParam("image") MultipartFile file, Model model) throws IOException {
+        String fileNames = uploadImage(model, file);
+        product.setImageUrl(fileNames);
         productService.addProduct(product);
-        return "redirect:/products";
-    }
-
-    @PutMapping("/products")
-    public String updateProduct(Product product) {
-        productService.updateProduct(product);
         return "redirect:/products";
     }
 
@@ -53,5 +67,28 @@ public class ProductController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+
+    public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Resource resource = resourceLoader.getResource(UPLOAD_DIRECTORY);
+        Path uploadPath = Paths.get(resource.getURI());
+        Path fileNameAndPath = uploadPath.resolve(file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        model.addAttribute("message", "Uploaded images: " + fileNames.toString());
+        return fileNames.toString();
+    }
+
+    private void addCategoriesToModel(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+    }
+
+    private void addProductsToModel(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        model.addAttribute("UPLOAD_DIRECTORY", UPLOAD_PATH);
     }
 }
